9a7eeca5de3259b68b053cb3acbbfdcf6fa3819b
pots <t.sato104@gmail.com>
2016-03-06 17:59:00 +0900

    first

---
+++ README.md
@@ -0,0 +1,20 @@
+# WPsB WP Nav Menu
+
+It is a wrapper of the procedure for use of the `wp_nav_menu`
+
+## Usage
+
+``` PHP
+$WPsB_WP_Nav_Menu = new WPsB_WP_Nav_Menu( $args );
+$WPsB_WP_Nav_Menu->register();
+```
+
+`$args`, See [wp_nav_menu() | Function | WordPress Developer Resources](https://developer.wordpress.org/reference/functions/wp_nav_menu/)
+
+## Feature
+
+* Unit Test
+
+## License
+
+GPLv2 or later
---
+++ WPsB_WP_Nav_Menu.php
@@ -0,0 +1,50 @@
+<?php
+
+/**
+ * Class WPsB_WP_Nav_Menu
+ *
+ * wp_nav_menu() | Function | WordPress Developer Resources https://developer.wordpress.org/reference/functions/wp_nav_menu/
+ */
+class WPsB_WP_Nav_Menu
+{
+	public $config;
+
+	/**
+	 * @param array $config
+	 */
+	public function __construct(array $config)
+	{
+		$defaults = array();
+		$this->config = array_merge($defaults, $config);
+
+		$this->register();
+	}
+
+	public function register()
+	{
+		add_action('after_setup_theme', array($this, 'after_setup_theme'), 10);
+	}
+
+	public function after_setup_theme()
+	{
+		$this->add_theme_support();
+		$this->register_nav_menus($this->config['register_nav_menus']);
+	}
+
+	public function add_theme_support()
+	{
+		add_theme_support('menus');
+	}
+
+	/**
+	 * @param array $menus
+	 *
+	 * @return void
+	 */
+	public function register_nav_menus(array $menus)
+	{
+		if (function_exists('register_nav_menus') && !empty($menus)) {
+			register_nav_menus($menus);
+		}
+	}
+}
